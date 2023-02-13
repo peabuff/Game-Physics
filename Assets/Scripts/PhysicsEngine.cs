4cee93c60b55cefff603c21fe4c4608186ec2669
Peabuff <peabufff@gmail.com>
2023-02-13 14:42:44 +0300

    Completed Newton's Second Law example

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -1,35 +1,38 @@
-using System;
-using System.Collections;
+
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PhysicsEngine : MonoBehaviour
 {
+    [SerializeField] public float mass = 1f;
     [SerializeField] public Vector3 velocityVector; // Average velocity this FixedUpdate()
     [SerializeField] public List<Vector3> forceVectorList = new List<Vector3>();
-    private Vector3 _netForceVector;
+    [SerializeField] [ReadOnlyInspector] public Vector3 netForceVector;
     private void FixedUpdate()
     {
         AddForces();
-        
-        if (_netForceVector == Vector3.zero)
-        {
-            transform.position += velocityVector * Time.deltaTime;
-        }
-        else
-        {
-            Debug.LogError("We have a net force = " +_netForceVector);
-        }
+        UpdateVelocity();
+
+        transform.position += velocityVector * Time.deltaTime;
     }
 
     private void AddForces()
     {
-        _netForceVector = Vector3.zero;
+        netForceVector = Vector3.zero;
         
         for (int i = 0; i < forceVectorList.Count; i++)
         {
-            _netForceVector += forceVectorList[i];
+            netForceVector += forceVectorList[i];
         }
+    }
+
+    private void UpdateVelocity()
+    {
+        //F = m x a;
+        //a = F / m;
+
+        Vector3 accelerationVector = netForceVector / mass;
+        velocityVector += accelerationVector * Time.deltaTime;
+
     }
 }
